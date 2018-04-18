3e542a1ffe5317d7764b9d87ac4b7da12f6afd21
Blake Schlin <blakeschlin@gmail.com>
2018-04-17 19:22:58 -0500

    add create action for favorites

--- app/controllers/v1/coins_controller.rb
+++ app/controllers/v1/coins_controller.rb
@@ -1,12 +1,14 @@
 class V1::CoinsController < ApplicationController
   def index
-    # Use Unirest with some API to get a list of coins
-
-    # print "Enter a subreddit:"
-    # input_word = gets.chomp
-    # response = Unirest.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=#{input_word}&tsyms=USD")
+    # Use Unirest with some API to get a list of coins (no user input required)
 
     render json: response.body
+  end
+
+  def show
+    response = Unirest.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD")
+    # get coin api id from user (params[:coin_api_id]...)
+
   end
 
   
--- app/controllers/v1/favorites_controller.rb
+++ app/controllers/v1/favorites_controller.rb
@@ -1,2 +1,15 @@
 class V1::FavoritesController < ApplicationController
+  def create
+    favorite = Favorite.new(
+      user_id: params[:user_id],
+      coin_name: params[:coin_name],
+      coin_api_id: params[:coind_api_id],
+      notes: params[:notes]
+    )
+    if favorite.save
+      render json: favorite.as_json
+    else 
+      render json: {errors: favorite.errors.full_messages}, status: 422
+    end
+  end
 end
